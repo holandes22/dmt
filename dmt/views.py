34b8714ff8f8192c354e6bf74265081f9b415e6a
Pablo Klijnjan <pabloklijnjan@il.ibm.com>
2012-04-26 14:15:06 +0300

    Adding sysinfo (not finished)

--- dmt/views.py
+++ dmt/views.py
@@ -1,2 +1,9 @@
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
+from dmtcore.os import platinfo
+
+def sysinfo(request):
+    return render_to_response('sysinfo.html', {'sysinfo': platinfo.get_platform_details()},
+                              context_instance = RequestContext(request))
 
