cef269bc417d91492eecb2c94cd7e9d54780cf62
Pablo Klijnjan <pabloklijnjan@il.ibm.com>
2012-04-26 13:55:56 +0300

    Cleaning up templates and urls

--- dmt/disks/urls.py
+++ dmt/disks/urls.py
@@ -1,15 +1,10 @@
 from django.conf.urls.defaults import patterns, url
-from dmt.disks.views import DiskView, AllDisksDetailsView
+from dmt.disks.views import AllDisksDetailsView
 from django.views.generic import ListView, DetailView
 from dmt.disks.models import Disk, MultipathDisk, Partition, Path
 
 urlpatterns = patterns('dmt.disks.views',
         url(r'force_refresh_disk_objects$', 'force_refresh_disk_objects', name='force_refresh_disk_objects'),
-        url(r'show_all_disks$', 'show_all_disks', name='show_all_disks'),
-        url(r'disk_list$', DiskView.as_view(), name='disk_list'),
-        url(r'disk_list2$', ListView.as_view(model=Disk, context_object_name='disks'), name='disk_list2'),
-        url(r'(?P<disk_id>\d+)/partitions', 'show_disk_partitions', name='show_disk_partitions'),
-        #####
         url(r'all/details$', AllDisksDetailsView.as_view(), name='all_disks_details'),
         url(r'basic/list$', ListView.as_view(model=Disk,
                                              template_name='disks/generic_list.html'), name='basic_disk_list'),
--- dmt/disks/views.py
+++ dmt/disks/views.py
@@ -1,34 +1,14 @@
 from django.db.models import permalink
-from django.template.context import RequestContext
-from django.shortcuts import render_to_response
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView
 
 from dmt.disks.tasks import refresh_disk_models
-from dmt.disks.models import Disk, Partition, MultipathDisk
+from dmt.disks.models import Disk, MultipathDisk
 
 def force_refresh_disk_objects(request):
     response = refresh_disk_models.delay()
     response.get()
-    return show_all_disks(request)
+    return AllDisksDetailsView.as_view()
 
-def show_all_disks(request):
-    disks = Disk.objects.all()
-    return render_to_response('disks.html', {'disks': disks},
-            context_instance=RequestContext(request))
-
-def show_disk_partitions(request, disk_id):
-    partitions = Partition.objects.filter(parent = disk_id)
-    return render_to_response('partitions.html', {'partitions': partitions},
-            context_instance = RequestContext(request))
-
-class DiskView(TemplateView):
-
-    template_name = 'disks/disk_list.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(DiskView, self).get_context_data(**kwargs)
-        context['disks'] = Disk.objects.all()
-        return context
     
 class AllDisksDetailsView(TemplateView):
     template_name = 'disks/all_disks_details.html'
