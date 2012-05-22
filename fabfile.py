c1a3e975b75c378d60a3ad0736a4584be03bda7e
Pablo Klijnjan <pablokl@il.ibm.com>
2012-05-22 18:31:56 +0300

    Adding Vagrant file

--- fabfile.py
+++ fabfile.py
@@ -1,1 +1,21 @@
+from fabric.api import env, local, run, cd, sudo
 
+
+
+def vagrant():
+    # change from the default user to 'vagrant'
+    env.user = 'vagrant'
+    # connect to the port-forwarded ssh
+
+    hostname = local('vagrant ssh-config | grep HostName', capture=True)
+    port = local('vagrant ssh-config | grep Port', capture=True)
+    env.hosts = ['{0}:{1}'.format(hostname.split()[1], port.split()[1]), ]
+
+    # use vagrant ssh key
+    result = local('vagrant ssh-config | grep IdentityFile', capture=True)
+    env.key_filename = result.split()[1]
+
+
+def pip_install():
+    with cd("/vagrant"):
+        sudo('pip install -r requirements.txt')
