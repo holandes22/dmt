2efc53d8bd711eb470b0bbb0ad34e54546c43a0f
Pablo Klijnjan <pablokl@il.ibm.com>
2012-05-23 13:31:09 +0300

    Adding templates to sudoers and celeryd.defaults
        - Added scripts in fabfile to render and place

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,30 @@
-from fabric.api import env, local, run, cd, sudo
+from string import Template
+from fabric.operations import prompt
+from fabric.api import env, local, run, cd, sudo, require
 
+
+def _replace_in_template(template_location, filename):
+    with open(template_location) as f:
+        template = Template(f.read())
+    with open(filename, 'w') as f:
+        f.write(template.substitute(**env))
+
+
+def replace_sudoers():
+    _replace_in_template('sudoers.template', '/home/pablo/Desktop/sudoers')
+
+    #backup original sudoers file
+    #replace with the template one
+
+def generate_celery_defaults():
+    _replace_in_template('celeryd.defaults.template', 'celeryd.defaults')
+
+def setup():
+    if not 'app_user' in env:
+        env.app_user = prompt("User to run the application")
+    require('app_user')
+    generate_celery_defaults()
+    replace_sudoers()
 
 
 def vagrant():
@@ -17,5 +42,17 @@ def vagrant():
 
 
 def pip_install():
-    with cd("/vagrant"):
+    with cd('/vagrant'):
         sudo('pip install -r requirements.txt')
+
+def syncdb():
+    with cd('/vagrant'):
+        run('python manage.py syncdb --noinput', pty=True)
+
+def start_celery():
+    with cd('/vagrant'):
+        run('./celeryd start')
+
+def runserver():
+    with cd('/vagrant'):
+        run('python manage.py runserver [::]:8000')
